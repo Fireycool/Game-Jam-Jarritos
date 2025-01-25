b5b6b8f8976d19331215bba9797a2ba9f50fffdf
Luis Alfonso Martinez Martinez <luisalfonsomartinezmartinez@outlook.es>
2025-01-25 16:02:04 -0600

    menu principal con fondo y placeholders

--- main_menu.gd
+++ main_menu.gd
@@ -1,14 +1,10 @@
 extends Control
 
-
-
-func _on_btn_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_1.tscn")
-
-
 func _on_btn_how_2_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://how2play.tscn")
 
-
 func _on_btn_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_btn_play_pressed() -> void:
+	pass # Replace with function body.
