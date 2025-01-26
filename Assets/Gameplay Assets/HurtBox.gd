3efa8fdf2422dbe488c4e473c5b476f1d3e660b1
Rock <rplh.edu@gmail.com>
2025-01-26 16:05:24 -0600

    End Goal

--- Assets/Gameplay Assets/HurtBox.gd
+++ Assets/Gameplay Assets/HurtBox.gd
@@ -4,8 +4,8 @@ extends Area2D
 @export var damage := 1
 
 func _init() -> void:
-	collision_layer = 8
-	collision_mask = 16
+	collision_layer = 32
+	collision_mask = 64
 
 func _ready() -> void:
 	connect("area_entered",self._on_area_entered)
