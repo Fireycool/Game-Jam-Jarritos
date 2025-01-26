08932bf22432eba85ca6a27e55fb9bb9235b7149
Luis Alfonso Martinez Martinez <luisalfonsomartinezmartinez@outlook.es>
2025-01-25 21:16:55 -0600

    Assets y 30% del mapa 1

--- Assets/Scenes/Main_Menu/main_menu.gd
+++ Assets/Scenes/Main_Menu/main_menu.gd
@@ -7,4 +7,4 @@ func _on_btn_exit_pressed() -> void:
 	get_tree().quit()
 
 func _on_btn_play_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Assets/Scenes/level_1/level_1.tscn")
